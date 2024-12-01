7cfe619532e0eddc4579b8a664735bb889be4cea
Pawel <pawmag.code@gmail.com>
2024-12-01 21:00:18 +0100

    Setup mvdm architecture, create view for Library, Collection and Character details

---
+++ app/src/main/java/com/wsb/comicsvoult/view/LibraryScreen.kt
@@ -0,0 +1,11 @@
+package com.wsb.comicsvoult.view
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+// Display Marvel API
+
+@Composable
+fun LibraryScreen() {
+    Text(text = "Library screen")
+}
