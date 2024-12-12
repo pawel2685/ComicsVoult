93050d96c1c59ee4a52a3e8ffc9837ffa8bdcc88
Pawel <pawmag.code@gmail.com>
2024-12-12 17:19:30 +0100

    Create Characters model class and Api call interface

---
+++ app/src/main/java/com/wsb/comicsvoult/model/api/MarvelApi.kt
@@ -0,0 +1,11 @@
+package com.wsb.comicsvoult.model.api
+
+import com.wsb.comicsvoult.model.CharactersApiResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MarvelApi {
+    @GET("characters")
+    fun getCharacters(@Query("nameSTartsWith") name: String): Call<CharactersApiResponse>
+}
