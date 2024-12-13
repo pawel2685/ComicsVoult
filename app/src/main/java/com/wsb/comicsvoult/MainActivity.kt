0ddeab71a721be80fa0d4da525463227363f214b
Pawel <pawmag.code@gmail.com>
2024-12-13 16:29:22 +0100

    Update manifest with needed permissions

--- app/src/main/java/com/wsb/comicsvoult/MainActivity.kt
+++ app/src/main/java/com/wsb/comicsvoult/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import dagger.hilt.android.AndroidEntryPoint
 
 import com.wsb.comicsvoult.ui.theme.ComicsVoultTheme
 import com.wsb.comicsvoult.view.CharactersBottomNav
@@ -28,6 +29,7 @@ sealed class Destination(val route: String) {
     }
 }
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
