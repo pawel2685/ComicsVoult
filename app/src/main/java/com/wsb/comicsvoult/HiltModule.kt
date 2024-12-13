169a36aa5e3c62f5a8067aa913a75fe40680f8e3
Pawel <pawmag.code@gmail.com>
2024-12-13 16:56:35 +0100

    Hilt implementation

---
+++ app/src/main/java/com/wsb/comicsvoult/HiltModule.kt
@@ -0,0 +1,14 @@
+package com.wsb.comicsvoult
+
+import com.wsb.comicsvoult.model.api.ApiService
+import com.wsb.comicsvoult.model.api.MarvelApiRepo
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+class HiltModule() {
+    @Provides
+    fun provideApiRepo() = MarvelApiRepo(ApiService.api)
+}
